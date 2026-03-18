6effef7b8f64cec1bafe83e5b3e144757bcad5c5
Tiago Rodrigues <tiagobb@hotmail.com>
2026-03-18 19:43:55 -0300

    ajuste responsividade para celular

--- services/google_sheets.py
+++ services/google_sheets.py
@@ -1,29 +1,24 @@
-# google_sheets.py
-import json
+# services/google_sheets.py
 import gspread
 import pandas as pd
 import streamlit as st
 from google.oauth2.service_account import Credentials
 
 def carregar_questoes(nome_planilha):
-    # Ler credenciais do Streamlit Secrets
-    cred_dict = st.secrets["gcp_service_account"]
+    # Criar cópia mutável do secret
+    cred_dict = dict(st.secrets["gcp_service_account"])
 
-    # Converter o private_key de múltiplas linhas
+    # Corrigir quebras de linha
     cred_dict["private_key"] = cred_dict["private_key"].replace("\\n", "\n")
 
-    # Escopos necessários
     scope = [
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
     ]
 
-    creds = Credentials.from_service_account_info(
-        cred_dict,
-        scopes=scope
-    )
-
+    creds = Credentials.from_service_account_info(cred_dict, scopes=scope)
     client = gspread.authorize(creds)
+
     sheet = client.open(nome_planilha)
     worksheet = sheet.sheet1
     dados = worksheet.get_all_records()
