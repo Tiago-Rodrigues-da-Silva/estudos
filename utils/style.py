b03a7058a5ad36704c0a08562b8de99245d812a5
Tiago Rodrigues <tiagobb@hotmail.com>
2026-03-18 18:19:03 -0300

    ajuste responsividade para celular

--- utils/style.py
+++ utils/style.py
@@ -5,11 +5,8 @@ def hide_streamlit_menu():
     st.markdown(
         """
         <style>
-        /* Esconde menu de hamburger / menu do diretório pages */
         #MainMenu {visibility: hidden;}
-        /* Esconde rodapé padrão do Streamlit */
         footer {visibility: hidden;}
-        /* Esconde cabeçalho Streamlit */
         header {visibility: hidden;}
         </style>
         """,
