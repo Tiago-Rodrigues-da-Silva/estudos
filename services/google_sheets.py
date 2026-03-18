2a0bd0a85ae56a4404294b4ae214ef706c640ed6
Tiago Rodrigues <tiagobb@hotmail.com>
2026-03-18 19:40:53 -0300

    ajuste responsividade para celular

--- services/google_sheets.py
+++ services/google_sheets.py
@@ -1,23 +1,25 @@
-import os
+# google_sheets.py
+import json
 import gspread
 import pandas as pd
+import streamlit as st
 from google.oauth2.service_account import Credentials
 
 def carregar_questoes(nome_planilha):
+    # Ler credenciais do Streamlit Secrets
+    cred_dict = st.secrets["gcp_service_account"]
+
+    # Converter o private_key de múltiplas linhas
+    cred_dict["private_key"] = cred_dict["private_key"].replace("\\n", "\n")
+
+    # Escopos necessários
     scope = [
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
     ]
 
-    # Caminho absoluto relativo a este arquivo
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # services/
-    CRED_PATH = os.path.join(BASE_DIR, "..", "credentials.json")  # sobe para raiz
-
-    if not os.path.exists(CRED_PATH):
-        raise FileNotFoundError(f"Arquivo de credenciais não encontrado: {CRED_PATH}")
-
-    creds = Credentials.from_service_account_file(
-        CRED_PATH,
+    creds = Credentials.from_service_account_info(
+        cred_dict,
         scopes=scope
     )
 
