48e2d608567a12156d086329f784ecaf22a39555
Tiago Rodrigues <tiagobb@hotmail.com>
2026-03-18 19:36:56 -0300

    ajuste responsividade para celular

--- services/google_sheets.py
+++ services/google_sheets.py
@@ -1,31 +1,30 @@
+import os
 import gspread
 import pandas as pd
-
 from google.oauth2.service_account import Credentials
 
-
 def carregar_questoes(nome_planilha):
-
-    scope=[
+    scope = [
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
     ]
 
-    creds=Credentials.from_service_account_file(
+    # Caminho absoluto relativo a este arquivo
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # services/
+    CRED_PATH = os.path.join(BASE_DIR, "..", "credentials.json")  # sobe para raiz
 
-        "credentials.json",
+    if not os.path.exists(CRED_PATH):
+        raise FileNotFoundError(f"Arquivo de credenciais não encontrado: {CRED_PATH}")
+
+    creds = Credentials.from_service_account_file(
+        CRED_PATH,
         scopes=scope
-
     )
 
-    client=gspread.authorize(creds)
-
-    sheet=client.open(nome_planilha)
-
-    worksheet=sheet.sheet1
-
-    dados=worksheet.get_all_records()
-
-    df=pd.DataFrame(dados)
+    client = gspread.authorize(creds)
+    sheet = client.open(nome_planilha)
+    worksheet = sheet.sheet1
+    dados = worksheet.get_all_records()
+    df = pd.DataFrame(dados)
 
     return df
